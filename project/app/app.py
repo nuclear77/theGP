ae5791baf6a712ed45ba7c513cc96a7c0b62610c
nuclear77 <spiritvoideu@gmail.com>
2025-07-05 17:28:21 +0300

    fix

--- project/app/app.py
+++ project/app/app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+import argparse
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello from DevOps!"
+    return "Hello from Dev1ps!"
 
+@app.route('/health')
+def health():
+    return "OK", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6060)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=6060, help='Port to run on')
+    args = parser.parse_args()
+    app.run(host='0.0.0.0', port=args.port)
